2fd239f7691d35cb92bb551ed901ca4bdda7755f
takzobye <takzobye@gmail.com>
2024-12-26 04:07:05 +0700

    feat(config/plugins.ts): add rest-cache

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => ({
+    "rest-cache": {
+        config: {
+            provider: {
+                name: "memory",
+                options: {
+                    max: 32767,
+                    maxAge: 3600,
+                },
+            },
+            strategy: {
+                contentTypes: [
+                    // list of Content-Types UID to cache
+                    "api::partner.partner",
+                    "api::sponsor.sponsor",
+                ],
+            },
+        },
+    },
+});
